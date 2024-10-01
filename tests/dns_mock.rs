25e366e6927db6d9ee5cea9d1028f40d7185c6da
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2024-10-01 04:03:19 -0400

    make some binaries more readable

--- tests/dns_mock.rs
+++ tests/dns_mock.rs
@@ -25,17 +25,12 @@ fn test_dns_mock() {
                     previous_message.as_ref().unwrap()[0],
                     previous_message.as_ref().unwrap()[1],
                 ];
-                response.extend_from_slice(&[
-                    0x81, 0x80, 0x00, 0x01, 0x00, 0x01, 0x00, 0x02, 0x00, 0x01, 0x03, 0x77, 0x77,
-                    0x77, 0x07, 0x65, 0x78, 0x61, 0x6d, 0x70, 0x6c, 0x65, 0x03, 0x63, 0x6f, 0x6d,
-                    0x00, 0x00, 0x01, 0x00, 0x01, 0xc0, 0x0c, 0x00, 0x01, 0x00, 0x01, 0x00, 0x01,
-                    0x08, 0xa4, 0x00, 0x04, 0x5d, 0xb8, 0xd8, 0x22, 0xc0, 0x10, 0x00, 0x02, 0x00,
-                    0x01, 0x00, 0x01, 0x08, 0xa3, 0x00, 0x14, 0x01, 0x61, 0x0c, 0x69, 0x61, 0x6e,
-                    0x61, 0x2d, 0x73, 0x65, 0x72, 0x76, 0x65, 0x72, 0x73, 0x03, 0x6e, 0x65, 0x74,
-                    0x00, 0xc0, 0x10, 0x00, 0x02, 0x00, 0x01, 0x00, 0x01, 0x08, 0xa3, 0x00, 0x04,
-                    0x01, 0x62, 0xc0, 0x3f, 0x00, 0x00, 0x29, 0x10, 0x00, 0x00, 0x00, 0x00, 0x00,
-                    0x00, 0x00,
-                ]);
+                response.extend_from_slice(
+                    b"\
+\x81\x80\x00\x01\x00\x01\x00\x02\x00\x01\x03www\x07example\x03com\x00\x00\x01\x00\x01\xc0\x0c\x00\x01\x00\x01\
+\x00\x01\x08\xa4\x00\x04\x5d\xb8\xd8\x22\xc0\x10\x00\x02\x00\x01\x00\x01\x08\xa3\x00\x14\x01a\x0ciana-servers\x03net\
+\x00\xc0\x10\x00\x02\x00\x01\x00\x01\x08\xa3\x00\x04\x01b\xc0?\x00\x00)\x10\x00\x00\x00\x00\x00\x00\x00",
+                );
                 Some(response)
             }),
             // Close the connection
@@ -66,11 +61,8 @@ fn test_dns_mock() {
     }
 
     assert_eq!(
-        vec![
-            1, 0, 0, 1, 0, 0, 0, 0, 0, 1, 3, 119, 119, 119, 7, 101, 120, 97, 109, 112, 108, 101, 3,
-            99, 111, 109, 0, 0, 1, 0, 1, 0, 0, 41, 4, 208, 0, 0, 0, 0, 0, 0
-        ],
-        dns_server_mocker.pop_received_message().unwrap()[2..]
+        b"\x01\x00\x00\x01\x00\x00\x00\x00\x00\x01\x03www\x07example\x03com\x00\x00\x01\x00\x01\x00\x00)\x04\xD0\x00\x00\x00\x00\x00\x00",
+        &dns_server_mocker.pop_received_message().unwrap()[2..]
     );
 
     // Check that no error has been raised by the mocked server
