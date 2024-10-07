dc4e8e86781e37408a6956a39941fa0140ee0548
Yuri Astrakhan <YuriAstrakhan@gmail.com>
2024-10-07 14:34:51 -0400

    Rework API to use just one struct
    * Now all mock socket instantiations are done with a specific object:
    
    ```rust
    // protocol-specific functions
    ServerMocker::tcp()
    ServerMocker::tcp_with_port(port)
    ServerMocker::udp()
    ServerMocker::udp_with_port(port)
    // generic function taking anything that
    // implements the needed (non-public) trait
    ServerMocker::new_with_opts(options_object)
    ```
    
    * Fix timeouts - by accident they had 100s instead of 100ms
    * Improve PostgreSQL tests with explicit configurations + timeouts + explicit communication stop (to prevent long waits when the client was shutting down)

--- src/udp_server.rs
+++ src/udp_server.rs
@@ -1,23 +1,20 @@
 use std::net::{SocketAddr, UdpSocket};
-use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
 use std::time::Duration;
 
+use crate::server_mocker::MockerOptions;
 use crate::Instruction::{
     self, ReceiveMessageWithMaxSize, SendMessage, SendMessageDependingOnLastReceivedMessage,
 };
-use crate::ServerMocker;
 use crate::ServerMockerError::{
     self, FailedToSendUdpMessage, GotSendMessageBeforeReceiveMessage, UnableToBindListener,
-    UnableToGetLocalAddress, UnableToReadUdpStream, UnableToSendInstructions,
-    UnableToSetReadTimeout,
+    UnableToGetLocalAddress, UnableToReadUdpStream, UnableToSetReadTimeout,
 };
 
-// FIXME: consider combining Udp and Tcp options into a single struct
-/// Options for the TCP server mocker
+/// Options for the UDP server mocker
 #[derive(Debug, Clone)]
-pub struct UdpMockerOptions {
+pub struct UdpMocker {
     /// Socket address on which the server will listen. Will be set to `127.0.0.1:0` by default.
     pub socket_addr: SocketAddr,
     /// Timeout for the server to wait for a message from the client.
@@ -28,138 +25,54 @@ pub struct UdpMockerOptions {
     pub max_packet_size: usize,
 }
 
-impl Default for UdpMockerOptions {
+impl Default for UdpMocker {
     fn default() -> Self {
         Self {
             socket_addr: SocketAddr::from(([127, 0, 0, 1], 0)),
             net_timeout: Duration::from_millis(100),
-            rx_timeout: Duration::from_secs(100),
+            rx_timeout: Duration::from_millis(100),
             max_packet_size: 65507,
         }
     }
 }
 
-/// A UDP server mocker
-///
-/// Can be used to mock a UDP server if the application you want to test uses UDP sockets to connect to a server.
-///
-/// It's preferable that only 1 client sends messages to the mocked server.
-/// When the object is dropped or a [stop instruction](Instruction::StopExchange) is received, the mocked server will stop.
-/// The server will also stop in case no more instructions are available.
-pub struct UdpServerMocker {
-    options: UdpMockerOptions,
-    socket_addr: SocketAddr,
-    instruction_tx: Sender<Vec<Instruction>>,
-    message_rx: Receiver<Vec<u8>>,
-    error_rx: Receiver<ServerMockerError>,
-}
-
-impl UdpServerMocker {
-    /// Create a new instance of the UDP server mocker on a random free port.
-    /// The port can be retrieved with the [`ServerMocker::port`] method.
-    pub fn new() -> Result<Self, ServerMockerError> {
-        Self::new_with_port(0)
-    }
-
-    /// Create a new instance of the UDP server mocker on the given port.
-    /// If the port is already in use, the method will return an error.
-    pub fn new_with_port(port: u16) -> Result<Self, ServerMockerError> {
-        let mut opts = UdpMockerOptions::default();
-        opts.socket_addr.set_port(port);
-        Self::new_with_opts(opts)
-    }
-
-    /// Create a new instance of the TCP server mocker with the given options.
-    ///
-    /// # Panics
-    /// It is assumed that threads can use messages channels without panicking.
-    pub fn new_with_opts(options: UdpMockerOptions) -> Result<Self, ServerMockerError> {
-        let (instruction_tx, instruction_rx) = mpsc::channel();
-        let (message_tx, message_rx) = mpsc::channel();
-        let (error_tx, error_rx) = mpsc::channel();
-
-        let listener = UdpSocket::bind(options.socket_addr)
-            .map_err(|e| UnableToBindListener(options.socket_addr, e))?;
-        let socket_addr = listener.local_addr().map_err(UnableToGetLocalAddress)?;
-
-        let options_copy = options.clone();
-        thread::spawn(move || {
-            UdpServerImpl {
-                options: options_copy,
-                connection: listener,
-                instruction_rx,
-                message_tx,
-                error_tx,
-            }
-            .handle_dgram_stream();
-        });
-
-        Ok(Self {
-            options,
-            socket_addr,
-            instruction_tx,
-            message_rx,
-            error_rx,
-        })
-    }
-
-    /// Get the options used to create the server mocker
-    pub fn options(&self) -> &UdpMockerOptions {
-        &self.options
-    }
-}
-
-/// `UdpServerMocker` implementation
-///
-/// # Example
-/// ```
-/// use std::net::{SocketAddr, UdpSocket};
-/// use socket_server_mocker::ServerMocker;
-/// use socket_server_mocker::Instruction::{ReceiveMessage, SendMessage, StopExchange};
-/// use socket_server_mocker::UdpServerMocker;
-///
-/// let server = UdpServerMocker::new().unwrap();
-/// // 0 = random port
-/// let mut client = UdpSocket::bind("127.0.0.1:0").unwrap();
-/// server.add_mock_instructions(vec![
-///    ReceiveMessage,
-///    SendMessage(vec![4, 5, 6]),
-///    StopExchange,
-/// ]).unwrap();
-///
-/// client.send_to(&[1, 2, 3], server.socket_address()).unwrap();
-/// let mut buffer = [0; 3];
-/// client.recv_from(&mut buffer).unwrap();
-/// assert_eq!([4, 5, 6], buffer);
-/// assert_eq!(Some(vec![1, 2, 3]), server.pop_received_message());
-/// assert!(server.pop_server_error().is_none());
-/// ```
-impl ServerMocker for UdpServerMocker {
+impl MockerOptions for UdpMocker {
     fn socket_address(&self) -> SocketAddr {
         self.socket_addr
     }
 
-    fn add_mock_instructions(
-        &self,
-        instructions: Vec<Instruction>,
-    ) -> Result<(), ServerMockerError> {
-        self.instruction_tx
-            .send(instructions)
-            .map_err(UnableToSendInstructions)
+    fn net_timeout(&self) -> Duration {
+        self.net_timeout
     }
 
-    fn pop_received_message(&self) -> Option<Vec<u8>> {
-        self.message_rx.recv_timeout(self.options.net_timeout).ok()
-    }
+    fn run(
+        self,
+        instruction_rx: Receiver<Vec<Instruction>>,
+        message_tx: Sender<Vec<u8>>,
+        error_tx: Sender<ServerMockerError>,
+    ) -> Result<SocketAddr, ServerMockerError> {
+        let connection = UdpSocket::bind(self.socket_addr)
+            .map_err(|e| UnableToBindListener(self.socket_addr, e))?;
+        let socket_addr = connection.local_addr().map_err(UnableToGetLocalAddress)?;
 
-    fn pop_server_error(&self) -> Option<ServerMockerError> {
-        self.error_rx.recv_timeout(self.options.net_timeout).ok()
+        thread::spawn(move || {
+            UdpServerImpl {
+                options: self,
+                connection,
+                instruction_rx,
+                message_tx,
+                error_tx,
+            }
+            .run();
+        });
+
+        Ok(socket_addr)
     }
 }
 
 /// TCP server mocker thread implementation
 struct UdpServerImpl {
-    options: UdpMockerOptions,
+    options: UdpMocker,
     connection: UdpSocket,
     instruction_rx: Receiver<Vec<Instruction>>,
     message_tx: Sender<Vec<u8>>,
@@ -168,7 +81,7 @@ struct UdpServerImpl {
 
 /// Specific implementation methods and constants for UDP server mocker
 impl UdpServerImpl {
-    fn handle_dgram_stream(&self) {
+    fn run(&self) {
         let timeout = Some(self.options.net_timeout);
         if let Err(e) = self.connection.set_read_timeout(timeout) {
             self.error_tx.send(UnableToSetReadTimeout(e)).unwrap();
